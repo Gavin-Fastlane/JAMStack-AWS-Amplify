c51cce7bcc57bc253349300e034a8a05096fb5de
李嘉伟 <jiawei.li@powervision.me>
2022-03-26 16:51:24 +0800

    集成prisimic

--- _data/homePage.js
+++ _data/homePage.js
@@ -1,9 +1,10 @@
-const client = require("../_server/client");
+const ApolloClient = require("../_server/client");
 const gql = require("graphql-tag");
-async function fetchUserData(username) {
+async function fetchHomePageData() {
     // do some async things
+    const client = await ApolloClient()
     let res = await client.query({
-        query: gql`
+        query: gql`{
             allIndexs{
               edges{
                 node{
@@ -37,16 +38,32 @@ async function fetchUserData(username) {
                 }
               }
             }
-        `
+          }`
       })
-    console.log("res===",res)
-    return username;
+   
+    return res.data;
   }
   
   module.exports = async function() {
-    let user1 = await fetchUserData("user1");
-  
-    return {
-        user1: "gggggg"
-    };
+    let res = await fetchHomePageData();
+    let homePageData = res.allIndexs.edges[0].node
+    let homePagePrimary = homePageData.body[0].primary
+    let homePageFields = homePageData.body[0].fields
+    let homePage = Object.assign({},{
+      page_title: homePageData.page_title[0].text,
+      page_description: homePageData.page_description[0].text,
+      body: Object.assign({},{
+        banner_title: homePagePrimary.banner_title[0].text,
+        banner_description: homePagePrimary.banner_description[0].text,
+        banner_btn_text: homePagePrimary.banner_btn_text[0].text,
+        banner_image: homePagePrimary.banner_image.url,
+        repeat_contents: homePageFields.map((field) => {
+          return Object.assign({},{
+            text: field.index_repeat_slice[0].text
+          })
+        })
+      })
+    })
+    console.log("res===",homePage)
+    return homePage;
   };
--- _server/client.js
+++ _server/client.js
@@ -1,16 +1,27 @@
-const createPrismicLink =  require("apollo-link-prismic").createPrismicLink;
-const InMemoryCache = require("apollo-cache-inmemory").InMemoryCache;
-const ApolloClient = require("@apollo/client/core").ApolloClient;
-// import gql from "graphql-tag";
+// global.fetch = require('node-fetch')
 
+async function getNodeFetch(params) {
+  const fetchModule = await import('node-fetch')
+  global.fetch = fetchModule.default
 
+  
+  const ApolloClient = require('@apollo/client/core').ApolloClient
+  const PrismicLink = require('apollo-link-prismic').PrismicLink
+  const InMemoryCache = require("apollo-cache-inmemory").InMemoryCache;
+  // import gql from "graphql-tag";
+  
+  
+  
+  global.client = global.client || new ApolloClient({
+    link: PrismicLink({
+      uri: "https://tech-hunt.prismic.io/graphql",
+      accessToken: "MC5ZajJYNHhFQUFDSUE4aUVv.77-977-9Le-_ve-_vRfvv71K77-977-977-977-9JDvvv71x77-9OkHvv719EHjvv73vv73vv73vv70gMFQzXw"
+    }),
+    cache: new InMemoryCache()
+  });
 
-global.client = global.client || new ApolloClient({
-  link: createPrismicLink({
-    uri: "https://tech-hunt.prismic.io/graphql",
-    accessToken: "MC5ZajJYNHhFQUFDSUE4aUVv.77-977-9Le-_ve-_vRfvv71K77-977-977-977-9JDvvv71x77-9OkHvv719EHjvv73vv73vv73vv70gMFQzXw"
-  }),
-  cache: new InMemoryCache()
-});
-
-module.exports = global.client;
+  return global.client
+}
+module.exports = async function (params) {
+  return await getNodeFetch()
+}; 
